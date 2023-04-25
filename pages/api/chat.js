0267cd63b016c3e3d527ca7193ec8209dd983f9f
Ihor Stefurak <opryshok@gmail.com>
2023-04-25 20:54:09 +0300

    push

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,18 +1,20 @@
-export default async function (req, res) {
+// Make sure to add OPENAI_API_KEY as a secret
 
-  const response = await fetch(process.env.LCC_ENDPOINT_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Api-Key": process.env.LCC_TOKEN
-    },
-    body: JSON.stringify({
-      question: req.body.question,
-      history: req.body.history
-    }),
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function(req, res) {
+  const completion = await openai.createChatCompletion({
+    // Replace `gpt-4` with `gpt-3.5-turbo` if you don't have early access to GPT-4
+    model: "gpt-4",
+    messages: [{ "role": "system", "content": "You are an expert in thinking, skilled in using a diverse collection of mental models for problem solving, decision making, and systems thinking. Your goal is to teach users how to think using mental models based on their unique life situations. First, categorize their situation according to mental model categorization. Then, guide users through their situations by suggesting a suitable mental model and providing step-by-step instructions. Upon completion, offer options to either list additional questions they can ask themselves and suggest names of other mental models that may be helpful for their situation." }].concat(req.body.messages)
+
   });
+  res.status(200).json({ result: completion.data.choices[0].message })
 
-    const data = await response.json();
-
-    res.status(200).json({ result: data })
 }
